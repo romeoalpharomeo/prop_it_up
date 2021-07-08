52f62641ea1a4792533605dd84e715aeded9bbee
romeoalpharomeo <ryanrey0333@gmail.com>
2021-07-08 18:18:12 -0400

    added state feature

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
     
     
 class PersonCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            birthday: this.props.age
+        };
+    }
+    
     render() {
-        const {firstName, lastName, age, hairColor} = this.props;
+
+        const {firstName, lastName, hairColor} = this.props;
+
         return (
             <div>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.birthday}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={() => this.setState({birthday: this.state.birthday + 1})}>Birthday Button for {firstName} {lastName} {this.state.birthday}</button>
             </div>
         );
     }
